e1eb3e6a1083ce87a414cc50fc6ed49b44cdc5ff
janis <jpostels@ethz.ch>
2021-12-10 16:03:59 +0100

    Create TB only on master GPU

--- train_ae.py
+++ train_ae.py
@@ -154,8 +154,9 @@ def main_worker(gpu, ngpus_per_node, args):
 
     print("training")
     # configure tensorboard logging
-    tb_path = os.path.join(config['logging_path'], 'log')
-    summary_writer = SummaryWriter(tb_path)
+    if gpu == 0:
+        tb_path = os.path.join(config['logging_path'], 'log')
+        summary_writer = SummaryWriter(tb_path)
 
     min_loss = 10000
     for epoch in range(cur_epoch, config['n_epochs']):
